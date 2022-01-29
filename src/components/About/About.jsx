7cca5c36c4d075b2502b96c1c39557d45d85801e
Evgeniy Kucherivaenko <draize26@gmail.com>
2022-01-30 00:17:57 +0700

    added Abour section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+import {SiHiveBlockchain, SiStrapi, SiFsecure} from "react-icons/si";
+import {VscServerProcess} from "react-icons/vsc";
+
+const About = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default About;
