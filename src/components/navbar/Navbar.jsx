92d17346c0d2206e4f17f577c5645f22ef4a2fc8
Evgeniy Kucherivaenko <draize26@gmail.com>
2022-01-30 00:15:00 +0700

    added section Hero.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
 import './Navbar.css';
+import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai";
 
 const Navbar = () => {
     const [click, setClick]=useState(false);
+
+    const handleClick = ()=> {
+        setClick(!click)
+    }
 
     return (
         <div className="navbar">
             <div className="container">
                 <h1 style={{marginLeft:'1rem', color: '#00d8ff'}}>Defi</h1>
-                <ul className="nav">
+                <ul className={click ? 'nav active' : 'nav'}>
                     <li className="nav-item">
                         <a href="/">Platform</a>
                     </li>
@@ -25,8 +30,8 @@ const Navbar = () => {
                         <a className="btn" href="/">Use Defi</a>
                     </li>
                 </ul>
-                <div className="hamburger">
-
+                <div onClick={handleClick} className="hamburger">
+                    {click ?  <AiOutlineClose className="icon"/> : <AiOutlineMenu className="icon"/>}
                 </div>
             </div>
         </div>
