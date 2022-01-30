fc89e1c09550f17676517146039294cd8ac18f17
Evgeniy Kucherivaenko <draize26@gmail.com>
2022-01-30 13:42:35 +0700

    added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from './components/hero/Hero';
 import About from "./components/About/About";
 import Developer from "./components/developer/Developer";
-
+import Subscribe from "./components/subcribe/Subscribe";
 const App = () => {
     return (
         <div>
@@ -11,6 +11,7 @@ const App = () => {
             <Hero/>
             <About/>
             <Developer/>
+            <Subscribe/>
         </div>
     );
 };
--- src/components/subcribe/Subscribe.jsx
+++ src/components/subcribe/Subscribe.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './subscribe.css';
 
 const Subscribe = () => {
     return (
-        <div>
-            
+        <div className="subscribe">
+            <div className="content">
+                <h2>Join Our Defi Community</h2>
+                <form action="">
+                    <div className="form-container display-col">
+                        <input type="email" placeholder="Enter your email"/>
+                        <button className="btn">Sign Up</button>
+                    </div>
+                    <div className="form-container">
+                        <input type="checkbox"/><p>Yes, I agree to receive email communications from Defi</p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
